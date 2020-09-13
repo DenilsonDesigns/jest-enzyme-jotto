db7ce78394560fa65a1122f5a60bf28d4867ba75
Daniel <daniellanguilller@gmail.com>
2020-09-13 19:44:28 +1000

    WIP: begin redux section

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
+import Congrats from "../Congrats/Congrats";
+import GuessedWords from "../GuessedWords/GuessedWords";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>App</h1>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Jotto</h1>
+        <Congrats success={true} />
+        <GuessedWords
+          guessedWords={[
+            {
+              guessedWord: "train",
+              letterMatchCount: 3,
+            },
+          ]}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
