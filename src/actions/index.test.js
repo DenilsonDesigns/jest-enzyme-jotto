de272d3fd8478c9ea465249ea4266c515b225fbe
Daniel <daniellanguilller@gmail.com>
2020-09-15 21:36:46 +1000

    WIP: redux first part

--- src/actions/index.test.js
+++
@@ -1,9 +0,0 @@
-import { correctGuess, actionTypes } from "./index";
-
-describe("correctGuess", () => {
-  test('returns an action with type "CORRECT_GUESS"', () => {
-    const action = correctGuess();
-    // *** for mutable type use toEqual
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-  });
-});
